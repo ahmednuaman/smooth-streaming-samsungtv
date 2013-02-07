53be6b0b0472ce600eb08a069887fcad7841aeec
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-02-07 11:21:27 +0000

    changed url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    // mediaURL: 'http://ecn.channel9.msdn.com/o9/content/smf/smoothcontent/elephantsdream/Elephants_Dream_1024-h264-st-aac.ism/manifest'
+    mediaURL: 'http://ecn.channel9.msdn.com/o9/content/smf/smoothcontent/elephantsdream/Elephants_Dream_1024-h264-st-aac.ism/manifest'
     // mediaURL: 'http://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest'
-    mediaURL: 'http://media.mokojotv.com/Thor_MSS_5P.ism/manifest'
+    // mediaURL: 'http://media.mokojotv.com/Thor_MSS_5P.ism/manifest'
 };
