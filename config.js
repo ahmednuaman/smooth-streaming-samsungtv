2d153242dfc7742d519790a4ef3165b38157d365
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-03-17 08:41:31 +0000

    updated url again

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    mediaURL: 'http://eu-download.acetrax.com.edgesuite.net/Movies/cabininthewoodstheLIONSHD_en_1280720169_35_25_20.wmv?purchaseId=B9C41B88-6C54-4CE2-937F-F4B5E4C38F0B&__gda__=1363551371_eb996a5a917b9357f0058277a7c60f5c'
+    mediaURL: 'http://eu-download.acetrax.com.edgesuite.net/Movies/cabininthewoodstheLIONSHD_en_1280720169_35_25_20.wmv?purchaseId=B9C41B88-6C54-4CE2-937F-F4B5E4C38F0B&__gda__=1363552871_538271ff085b2f457496a7e87945a691|COMPONENT=WMDRM'
     // mediaURL: 'http://ecn.channel9.msdn.com/o9/content/smf/smoothcontent/elephantsdream/Elephants_Dream_1024-h264-st-aac.ism/manifest'
     // mediaURL: 'http://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest'
     // mediaURL: 'http://media.mokojotv.com/Thor_MSS_5P.ism/manifest'
